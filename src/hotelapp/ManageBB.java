f3a97fa4963017845efc6c24b752952a5fc3548f
robertsoldan <x18107800@student.ncirl.ie>
2019-11-22 12:13:17 +0000

    Boilerplate Commit

---
+++ src/hotelapp/ManageBB.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelapp;
+
+/**
+ *
+ * @author robert
+ */
+public class ManageBB extends ManageRoom{
+    public ManageBB() {
+        super();
+    }
+}
